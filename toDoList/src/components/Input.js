b2c8ea7948761aeba3be31561a2fafa2da5a80bb
Merve Erdogmus <merve.erdogmus@adcolony.com>
2020-08-15 00:25:03 +0300

    Added to-do details component and binding data

---
+++ toDoList/src/components/Input.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {TextInput} from 'react-native';
+
+const Input = (props) => {
+   
+return (
+  
+  <TextInput
+    placeholder={props.placeholder}
+    placeholderTextColor = "white"
+    value = {props.value}
+    multiline
+    numberOfLines={4}
+    onChangeText = {(value) => props.changeText(value)}
+    style={[
+      { flex: 1,
+        backgroundColor: 'pink',
+        color: 'white',
+        width: '85%',
+        marginTop: '7%',
+        alignContent: 'center',
+        justifyContent: 'center',
+        textAlign:'center',
+      },
+      props.style,
+    ]}></TextInput>
+);}
+
+export default Input;
--- toDoList/src/screen/ToDoDetails.js
+++ toDoList/src/screen/ToDoDetails.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-
+import React, {useState} from 'react';
+import {StyleSheet, Text, View, Button, ScrollView} from 'react-native';
+import Input from '../components/Input'
 const ToDoDetails = (props) => {
- 
-    return (
-<Button title = {'Save'}onPress = {()=> {props.navigation.navigate('Home')}}>
-</Button>
-    );
-}
 
+ const [title, setTitle] = useState();
+ const [note, setNote] = useState();
+  return (
+    <ScrollView>
+        <View  style={{alignItems: 'center', justifyContent: 'center', flex: 1, }}>
+    <Input value = {title} placeholder = 'Title' style = {styles.title} changeText = {(value)=> setTitle(value)} />
+    <Input value = {note} placeholder = 'Notes' style = {styles.note} changeText = {(value)=> setNote(value)} />
+      <Button
+        title={'Save'}
+        onPress={() => {
+          props.navigation.navigate('Home');
+        }}></Button>
+        </View>
+    </ScrollView>
+  );
+};
+const styles = {
+    title : { fontSize :40, fontWeight : 'bold'},
+    note : {fontSize: 25}
+}
 export default ToDoDetails;
