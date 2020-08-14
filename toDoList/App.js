0709401c7da6e66c04779a656aceb70ca369a9a9
Merve Erdogmus <merve.erdogmus@adcolony.com>
2020-08-14 21:25:45 +0300

    Added basic flatlist

--- toDoList/App.js
+++ toDoList/App.js
@@ -1,54 +1,61 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-  Flatlist
-} from 'react-native';
+import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar, KeyboardAvoidingView } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-
-} from 'react-native/Libraries/NewAppScreen';
-
-
-const App: () => React$Node = () => {
-
-  const renderItem = ({item}) => (
-    <View style={styles.item}>
-      <Text style = {styles.title}>{item.name}</Text>
-      <Text style = {styles.title}>{item.status}</Text>
-    </View>
-  )
-
+const DATA = [
   
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-     
-      </SafeAreaView>
-    </>
+];
+
+const Item = ({ title }) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
+
+const App = () => {
+
+  const renderItem = ({ item }) => (
+    <Item title={item.title} />
   );
-};
 
-const styles = {
-
+  return (
+    <SafeAreaView style={styles.container}>
+       <KeyboardAvoidingView
+       behavior={Platform.OS == 'ios' ? 'padding' : 'height'}
+        style={{flex: 1}}>
+      <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+        ListEmptyComponent={() => {
+          return (
+            <View style={{alignItems: 'center', margin: 70}}>
+              <Text style={{fontSize: 20, fontWeight: 'bold'}}>
+                Please enter any to-do to see what is going on.
+              </Text>
+              
+            </View>
+          );
+        }}
+      />
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
+});
 
 export default App;
